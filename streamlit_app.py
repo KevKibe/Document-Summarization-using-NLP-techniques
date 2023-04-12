47aaff467503990e880afcf88438183881c48fb0
Kevin Kibe <kchegz234@gmail.com>
2023-04-12 19:35:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,45 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
-def summarize(content):
-    # Load the abstractive summarization model and tokenizer
+def generate_summaries(content_list):
+    """
+    Generates summaries for a list of news articles.
+
+    Args:
+    - content_list (List[str]): A list of news article content.
+
+    Returns:
+    - List[str]: A list of summaries generated for each news article.
+    """
+    print('Length of content list:', len(content_list))
+    # Initialize the tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained("t5-base")
     model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
 
-    # Preprocess the news article by encoding it as input_ids and attention_mask
-    inputs = tokenizer(content, padding=True, truncation=True, max_length=512, return_tensors='pt')
+    summaries = []
+    for content in content_list:
+        inputs = tokenizer(content, padding=True, truncation=True, max_length=512, return_tensors='pt')
+        outputs = model.generate(inputs['input_ids'], attention_mask=inputs['attention_mask'], max_length=150, num_beams=4, early_stopping=True)
+        summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        summaries.append(summary)
 
-    # Generate summary
-    outputs = model.generate(inputs['input_ids'], attention_mask=inputs['attention_mask'], max_length=150, num_beams=4, early_stopping=True)
+    print('Length of the summarized article:', len(summaries[0]))
+    print(summaries)
 
-    # Decode the summary from the output_ids
-    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return summaries
 
-    return summary
+
     # Set the app title
 st.title("Large text Summarizer")
 
     # Create a text input for entering the news article content
-content = st.text_area("Enter the large text:", height=200)
+content_list = st.text_area("Enter the large text:", height=200)
 
     # Create a button for generating the summary
 if st.button("Generate Summary"):
-  if content:
+  if content_list:
             # Call the summarize function to generate the summary
-      summary = summarize(content)
+      summary = summarize(content_list)
 
             # Display the summary
       st.subheader("Summary:")
