586211025cd31780a09421880257c4230aac1259
Kevin Kibe <kchegz234@gmail.com>
2023-04-13 17:29:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,38 @@ def generate_summaries(content_list):
     print(summaries)
 
     return summaries
+PAGE_STYLE = """
+<style>
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f8f9fa;
+    margin: 0;
+    padding: 0;
+}
 
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+.title {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+}
+
+.button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 12px 20px;
+    border: none;
+    cursor: pointer;
+    font-size: 16px;
+    margin-top: 16px;
+
+</style>
+"""
 # Set the app title
 st.title("Large Text Summarizer")
 
