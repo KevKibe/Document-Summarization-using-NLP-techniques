2da45eb760ba9283acf1d9c61c0b1a622550f72a
Kevin Kibe <kchegz234@gmail.com>
2023-04-13 17:23:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import BartTokenizer, BartForConditionalGeneration
 
-tokenizer = AutoTokenizer.from_pretrained("t5-base")
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
 
 def generate_summaries(content_list):
     """
@@ -16,12 +16,11 @@ def generate_summaries(content_list):
     """
     print('Length of content list:', len(content_list))
     # Initialize the tokenizer and model
-    
 
     summaries = []
     for content in content_list:
         inputs = tokenizer(content, padding=True, truncation=True, max_length=512, return_tensors='pt')
-        outputs = model.generate(inputs['input_ids'], attention_mask=inputs['attention_mask'], max_length=150, num_beams=4, early_stopping=True)
+        outputs = model.generate(inputs.input_ids, attention_mask=inputs.attention_mask, max_length=150, num_beams=4, length_penalty=2.0)
         summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
         summaries.append(summary)
 
@@ -30,23 +29,21 @@ def generate_summaries(content_list):
 
     return summaries
 
+# Set the app title
+st.title("Large Text Summarizer")
 
-    # Set the app title
-st.title("Large text Summarizer")
-
-    # Create a text input for entering the news article content
+# Create a text input for entering the news article content
 content_list = st.text_area("Enter the large text:", height=200)
 
-    # Create a button for generating the summary
+# Create a button for generating the summary
 if st.button("Generate Summary"):
-  if content_list:
-            # Call the summarize function to generate the summary
-      summary = generate_summaries(content_list)
+    if content_list:
+        # Call the generate_summaries function to generate the summary
+        summaries = generate_summaries([content_list])
 
-            # Display the summary
-      st.subheader("Summary:")
-      st.write(summary)
-      st.success("Summary generated successfully!")
-  else:
-      st.warning("Please enter some content for summarization.")
-    
+        # Display the summary
+        st.subheader("Summary:")
+        st.write(summaries[0])
+        st.success("Summary generated successfully!")
+    else:
+        st.warning("Please enter some content for summarization.")
