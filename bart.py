6cb8b7f12e36c7207e2aa89e8b709b82d41e694b
Kevin Kibe <kchegz234@gmail.com>
2023-04-24 19:04:15 +0300

    Update bart.py

--- bart.py
+++ bart.py
@@ -1,19 +1,19 @@
 import transformers
 from transformers import BartTokenizer, BartForConditionalGeneration
 
-def t5_summary(INCONTEXT: str) -> str:
+def bart_summary(INCONTEXT: str) -> str:
     """
-    Summary of T5 model.
+    Summary of BART model.
     
     """
-    # Load T5 model
-    model = AutoModelForSeq2SeqLM.from_pretrained('t5-base')
-    tokenizer = AutoTokenizer.from_pretrained('t5-base')
+    # Load BART model
+    model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
+    tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 
     ARTICLE = INCONTEXT
-    inputs = tokenizer("summarize" + ARTICLE,
+    inputs = tokenizer("summarize: " + ARTICLE,
                        return_tensors="pt",
-                       max_length=512,
+                       max_length=1024,
                        truncation=True,
                        padding="max_length")
     outputs = model.generate(
