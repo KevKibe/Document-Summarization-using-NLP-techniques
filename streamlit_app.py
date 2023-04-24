c87d260952a7608f177361fde330500c413702fa
Kevin Kibe <kchegz234@gmail.com>
2023-04-24 19:16:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,15 @@ st.title("Large Text Summarizer")
 
 # Create a text input for entering the news article content
 text = st.text_area("Enter the large text:", height=200)
-
-# Create a button for generating the summary
+# Check if the "Generate Summary" button is clicked
 if st.button("Generate Summary"):
+    # If so, generate a summary using the t5_summary() function
     summary = t5_summary(text)
-   st.subheader("Summary:")
-   st.success(summary)
+    
+    # Display the summary as a subheader and highlight it in green
+    st.subheader("Summary:")
+    st.success(summary)
 else:
-   st.warning("Please enter some content for summarization.")
+    # If the button is not clicked, show a warning message
+    st.warning("Please enter some content for summarization.")
+
